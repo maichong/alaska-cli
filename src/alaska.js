a99cfb550954a991282533eefecc50ef1951f360
Liang Xingchen <liang@maichong.it>
2016-03-02 18:27:25 +0800

    init

---
+++ src/alaska.js
@@ -0,0 +1,16 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-03-02
+ * @author Liang <liang@maichong.it>
+ */
+
+'use strict';
+
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .command('create [name]', 'create project')
+  .command('install [service]', 'install a service')
+  .command('build-admin', 'build admin views')
+  .parse(process.argv);
