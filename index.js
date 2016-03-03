87f0e2735c4a9c8eac3c9cfbe8e35ca2a8cace85
Li Yudeng <li@maichong.it>
2016-03-03 17:47:34 +0800

    alaska-build   or   build

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * 脉冲软件
+ * http://maichong.it
+ * Created by LiYuDeng on 2016/3/3.
+ * li@maichong.it
+ */
+'use strict';
+const program = require('commander');
+const buildAdmin = require('./src/alaska-build');
+program
+  .version('0.1.0')
+  .command('alaska-build')
+  .alias('build')
+  .description('build admin views')
+  .action(buildAdmin);
+
+program.parse(process.argv);
